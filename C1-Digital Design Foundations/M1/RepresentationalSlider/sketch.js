6c8129839a5c0d77e4dc88b165779dacbe055c72
Claire Spain <35749695+ClarissaSpain@users.noreply.github.com>
2021-09-12 23:48:10 -0500

    M1 Realism to Abstract Slider

---
+++ C1-Digital Design Foundations/M1/RepresentationalSlider/sketch.js
@@ -0,0 +1,49 @@
+const NUM_IMGS = 6,
+  imgs = [];
+
+//changes the size of the canvas and the images
+let w=1920/2;
+let h=1080/2;
+
+function preload() {
+  for (let i = 1; i < NUM_IMGS; i++) {
+   imgs[i] = loadImage('./preload/cat0'+ [i] +'.png');
+  }
+}
+
+function setup() {
+  createCanvas(w, h);
+  setupSlider();
+  // textFont("Glory");
+  setupText();
+  
+}
+
+function draw() {
+  image(imgs[slider.value()], 0, 0, w, h);
+  
+  
+}
+
+const setupSlider = _ => {
+  // textSize(22);
+  // text("Realism", 25, height+1);
+  // fill(0);
+  // text("Abstract", 25, 90);
+  // fill(0);
+  let r = createP('REALISM');
+  r.style('font-size', '24');
+  r.style('font-family', 'Glory');
+  r.position(width*0.5 - 140, height + 3)
+  slider = createSlider(1, 5, 1);
+  slider.position(width*0.5 - 75, height + 20);
+  let a = createP('ABSTRACT');
+  a.style('font-size', '24');
+  a.style('font-family', 'Glory');
+  a.position(width*0.5 + 70, height + 3)
+  
+};
+
+function setupText(){
+
+}
