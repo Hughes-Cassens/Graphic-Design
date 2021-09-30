7ce173d3967a58e6120ab4da1aa260f3b693748d
Spencer Buffum <46574476+sbuff25@users.noreply.github.com>
2021-09-30 08:10:57 -0600

    Updated event listener

--- C1-Digital Design Foundations/M1/AbstractBlur/script.js
+++ C1-Digital Design Foundations/M1/AbstractBlur/script.js
@@ -1,21 +1,21 @@
-// var blurSlider = document.getElementById('slider');
-// var blur = document.getElementsByClassName('blur')[0];
+var blurSlider = document.getElementById('slider');
+var blur = document.getElementsByClassName('blur')[0];
 var blurVal;
 
-// blurSlider.addEventListener("change", function() {
-//   blur.style.filter = "blur("+this.value+"px)";
-//   console.log(this.value);
-// })
-
-$("#slider").slider({
-  value: 0,
-  min: 0,
-  max: 50,
-  slide: handleSlideChange
+blurSlider.addEventListener("input", function() {
+  blur.style.filter = "blur("+this.value+"px)";
+  console.log(this.value);
 })
 
-function handleSlideChange(event, slider){
-  blurVal = slider.value;
-  $("#image").css('filter','blur('+ blurVal +'px)');
-  console.log(blurVal);
-}
+// $("#slider").slider({
+//   value: 0,
+//   min: 0,
+//   max: 50,
+//   slide: handleSlideChange
+// })
+
+// function handleSlideChange(event, slider){
+//   blurVal = slider.value;
+//   $("#image").css('filter','blur('+ blurVal +'px)');
+//   console.log(blurVal);
+// }
