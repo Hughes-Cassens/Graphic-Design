3643f91d6a5a6cb6b2892e245127d91850a47267
Spencer Buffum <46574476+sbuff25@users.noreply.github.com>
2021-10-05 12:02:22 -0600

    Update layerPanel.js

--- C1-Digital Design Foundations/M2/layerV2/layerPanel.js
+++ C1-Digital Design Foundations/M2/layerV2/layerPanel.js
@@ -18,6 +18,19 @@ var recLayerVis = new Konva.Rect({
     strokeWidth: 4,
     draggable: true,
 })
+//Layer Text
+var recLayerText = new Konva.Text({
+    x:100,
+    y:100,
+    text: 'Rectangle',
+    fontSize: 30,
+    fontFamily: 'Calibri',
+    fill: 'black',
+    draggable: true,
+})
+
+
+
 var lineLAyerVis = new Konva.Rect({
     x:100,
     y:500,
@@ -28,8 +41,28 @@ var lineLAyerVis = new Konva.Rect({
     strokeWidth: 4,
     draggable: true, 
 })
-panelLayer.add(lineLAyerVis);
+// Group Text and rectangles together
+const rectangleLayerGroup = new Konva.Group();
+
+rectangleLayerGroup.add(recLayerVis);
+rectangleLayerGroup.add(recLayerText);
+
+
+
+// layer z-index
+const recLayerPosition = recLayerText.zIndex();
+recLayerText.zIndex(1)
+
+
+
+
+
 panelLayer.add(recLayerVis);
+panelLayer.add(recLayerText);
+panelLayer.add(rectangleLayerGroup);
+
+panelLayer.add(lineLAyerVis);
+
 layerStage.add(panelLayer);
 
 
@@ -51,6 +84,7 @@ function fitStageIntoLayerPanel() {
     layerStage.scale({ x: scale, y: scale });
   }
   
+  console.log(rectangleLayerGroup);
   fitStageIntoLayerPanel();
   // adapt the stage on any window resize
   window.addEventListener('resize', fitStageIntoLayerPanel);
