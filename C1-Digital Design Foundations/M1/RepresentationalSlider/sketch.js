2263374bcd5f7fd74763a5a9c78f29bede8585bd
Claire Spain <35749695+clairespain@users.noreply.github.com>
2021-10-26 22:27:01 -0500

    Eyedropper | Abstract Blur | Representational Slider
    + Replaced Image in Abstract Blur
    - Removed P5 script/canvas in Rep Slider
    +Replaced with Html CSS JS for hopefully faster load time.

--- C1-Digital Design Foundations/M1/RepresentationalSlider/sketch.js
+++ C1-Digital Design Foundations/M1/RepresentationalSlider/sketch.js
@@ -1,34 +1,53 @@
-const NUM_IMGS = 6,
-  imgs = [];
+// const NUM_IMGS = 6,
+//   imgs = [];
 
-//changes the size of the canvas and the images
-let w=1920/2;
-let h=1080/2;
+// //changes the size of the canvas and the images
+// let w=1920/3;
+// let h=1080/3;
 
-function preload() {
-  for (let i = 1; i < NUM_IMGS; i++) {
-   imgs[i] = loadImage('./preload/cat0'+ [i] +'.png');
+// function preload() {
+//   for (let i = 1; i < NUM_IMGS; i++) {
+//    imgs[i] = loadImage('./preload/cat0'+ [i] +'.png');
+//   }
+// }
+
+// function setup() {
+//   createCanvas(w, h);
+//   setupSlider();
+// }
+
+// function draw() {
+//   image(imgs[slider.value()], 0, 0, w, h);
+// }
+
+// const setupSlider = _ => {
+//   let r = createP('REALISM');
+//   r.style('font-size', '24');
+//   r.style('font-family', 'Glory');
+//   r.position(width*0.5 - 140, height + 3)
+//   slider = createSlider(1, 5, 1);
+//   slider.position(width, height + 20);
+//   let a = createP('ABSTRACT');
+//   a.style('font-size', '24');
+//   a.style('font-family', 'Glory');
+//   a.position(width*0.5 + 70, height + 3)
+// };
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  x[slideIndex-1].style.display = "block";
 }
-
-function setup() {
-  createCanvas(w, h);
-  setupSlider();
-}
-
-function draw() {
-  image(imgs[slider.value()], 0, 0, w, h);
-}
-
-const setupSlider = _ => {
-  let r = createP('REALISM');
-  r.style('font-size', '24');
-  r.style('font-family', 'Glory');
-  r.position(width*0.5 - 140, height + 3)
-  slider = createSlider(1, 5, 1);
-  slider.position(width*0.5 - 75, height + 20);
-  let a = createP('ABSTRACT');
-  a.style('font-size', '24');
-  a.style('font-family', 'Glory');
-  a.position(width*0.5 + 70, height + 3)
-};
